aedaaffc24bfc47aa1052f796d5dc3bdf8030a76
Silver_Hsiao <silver041692@gmail.com>
2024-09-24 00:36:37 +0900

    fix page

--- composeApp/src/jsMain/kotlin/main.kt
+++ composeApp/src/jsMain/kotlin/main.kt
@@ -2,12 +2,16 @@ package cc.bap5
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
+import androidx.compose.ui.window.ComposeViewport
+import kotlinx.browser.document
 import org.jetbrains.skiko.wasm.onWasmReady
 
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     onWasmReady {
-        CanvasBasedWindow(canvasElementId = "ComposeTarget") { App() }
+        ComposeViewport(document.body!!) {
+            App()
+        }
     }
 }
