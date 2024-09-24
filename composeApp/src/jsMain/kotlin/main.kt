f1b56634377a2c96da959cf9a9177112a00afa57
Silver_Hsiao <silver041692@gmail.com>
2024-09-24 20:20:09 +0900

    adjust page

--- composeApp/src/jsMain/kotlin/main.kt
+++ composeApp/src/jsMain/kotlin/main.kt
@@ -11,6 +11,8 @@ import org.jetbrains.skiko.wasm.onWasmReady
 fun main() {
     onWasmReady {
         ComposeViewport(document.body!!) {
+            val loading = document.getElementById("loading")!!
+            loading.remove()
             App()
         }
     }
